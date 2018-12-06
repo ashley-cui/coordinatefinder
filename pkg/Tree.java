9e1f0e96d4be8515f497ee1cee8aac443b5a8e4e
srieger <srieger256@gmail.com>
2018-12-06 16:13:17 -0500

    Add test tree

--- pkg/Tree.java
+++ pkg/Tree.java
@@ -32,12 +32,17 @@ public class Tree {
     }
 
     public static void Insert(Coordinate c) {
-    	root = insert(root, c, true);
+    	if (root == null) {
+    		root = insert(root, c, true);
+    	}
+    	else {
+    		insert(root, c, true);
+    	}
     }
 
  	public static Node insert(Node node, Coordinate c, boolean evenLevel) {
  		if (node==null) {
- 			Node n = new Tree.Node(c.lon, c.lat, c.county, !evenLevel);
+ 			Node n = new Node(c.lon, c.lat, c.county, !evenLevel);
  			return n;
  		}
  		else if (comparePoints(node, c, evenLevel)) {
@@ -52,6 +57,27 @@ public class Tree {
  	}
 
 	public static void main(String[] args) {
+
+		Tree t = new Tree();
+
+		t.Insert(new Coordinate(100.0,10.0,"County"));
+		t.Insert(new Coordinate(50.0,200.0,"County"));
+		t.Insert(new Coordinate(250.0,7.0,"County"));
+		t.Insert(new Coordinate(75.0,4.0,"County"));
+		t.Insert(new Coordinate(27.0,400.0,"County"));
+		t.Insert(new Coordinate(470.0,2.0,"County"));
+		t.Insert(new Coordinate(150.0,82.0,"County"));
+
+		System.out.printf("should be 100 %f\n", root.data.lat);
+		System.out.printf("should be 50 %f\n", root.leftChild.data.lat);
+		System.out.printf("should be 250 %f\n", root.rightChild.data.lat);
+		System.out.printf("should be 75 %f\n", root.leftChild.leftChild.data.lat);
+		System.out.printf("should be 27 %f\n", root.leftChild.rightChild.data.lat);
+		System.out.printf("should be 470 %f\n", root.rightChild.leftChild.data.lat);
+		System.out.printf("should be 150 %f\n", root.rightChild.rightChild.data.lat);
+
+
+
 		// File file = new File("TestData.txt"); 
 
 		// BufferedReader br = new BufferedReader(new FileReader(file)); 
