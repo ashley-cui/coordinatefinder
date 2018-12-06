7253a2439ff725e0e82db812fb572d05f3ce7b64
srieger <srieger256@gmail.com>
2018-12-06 13:23:27 -0500

    Outlined code for inserting new nodes

--- Tree.java
+++ Tree.java
@@ -1,28 +1,57 @@
+import java.lang.Math
+
 public class Tree {
 
 	private static class Node {
 		Coordinate data;
 		Node leftChild;
 		Node rightChild;
-		int depth;
+		boolean evenLevel;
 
-		public Node(double x, double y, int depth) {
-			data = new Coordinate(x, y);
+		public Node(double x, double y, String county, boolean evenLevel) {
+			data = new Coordinate(x, y, county);
 			leftChild = null;
 			rightChild = null;
-			depth = depth;
-
-			public Coordinate(double x, double y, String county) {
-				xCoor = x;
-				yCoor = y;
-				county = county;
-			}
+			evenLevel = evenLevel;
 		}
+	}
+
+	public Coordinate(double lon, double lat, String county) {
+		coordPair = Haversine(lon, lat)[0];
+		county = county;
 	}
  	
 	int dimensions = 2;
 
  	private Node root;
+
+ 	public static int[] Haversine(double longitude, double latitude) {
+ 		coordPair = new int[]{0, 0};
+
+ 	}
+
+ 	public boolean comparePoints(Node n, Coordinate c, boolean evenLevel) {
+        if (evenLevel) {
+            return c.x() > n.data.x();
+        }
+        else return c.y() > n.data.y();
+    }
+
+    public static void Insert(Coordinate c) {
+    	root = insert(root, c, True);
+    }
+
+ 	public static void insert(Node node, Coordinate c, boolean evenLevel) {
+ 		if (n==null) {
+ 			return new Node(c.lon, c.lat, c.county, !evenLevel);
+ 		}
+ 		else if (comparePoints(node, c, evenLevel) == True) {
+ 			//insert as right child
+ 		} 
+ 		else if (comparePoints(node, c, evenLevel) == False) {
+ 			//insert as left child
+ 		}
+ 	}
 
 	public static void main(String[] args) {
 		
